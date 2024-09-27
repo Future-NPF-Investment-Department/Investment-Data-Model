c4f880a336abd45882c4457f0bb2b84ce8314f2b
kinest22 <nirvana897@gmail.com>
2024-09-27 12:16:41 +0300

    PathsInfo class removed from the scope of the project

--- src/Context/Entities/ReportSourceFile.cs
+++ src/Context/Entities/ReportSourceFile.cs
@@ -70,18 +70,6 @@ namespace InvestmentDataModel.Context.Entities
         /// </remarks>
         public virtual ICollection<AssetFlow> FlowsRecords { get; set; } = null!;
 
-        /// <summary>
-        ///     Creates new instance of <see cref="ReportSourceFile"/>.
-        /// </summary>
-        public static ReportSourceFile New(FileInfo file, PathsInfo pathsInfo)
-        {
-            if (file.Directory is null) throw new Exception("");
-            ReportPricingType pricing = pathsInfo.PricingFlags[file.Directory!.FullName];
-            SqlTargetTable destination = pathsInfo.SqlTargetFlags[file.Directory!.FullName];
-            string provider = pathsInfo.ProviderFlags[file.Directory!.FullName];
-            return new ReportSourceFile(file, provider, pricing, destination);
-        }
-
         public static ReportSourceFile New(FileInfo file, string fileProvifer, ReportPricingType pricing, SqlTargetTable destination)
         {
             if (file.Directory is null) throw new Exception("");
